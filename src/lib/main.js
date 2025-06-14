d2af2192729d0b025de82c01d06b48a3a302495e
stiehle <gstiehle@yahoo.de>
2025-06-14 14:58:51 +0200

    Add Appwrite main

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,23 +1,13 @@
 import { Client, Users } from "node-appwrite";
 
 // This Appwrite function will be executed every time your function is triggered
-export default async ({ req, res, log, error }: any) => {
+export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   const client = new Client()
-    .setEndpoint("https://fra.cloud.appwrite.io/v1")
-    .setProject(
-      process.env.APPWRITE_PROJECT_ID ??
-        (() => {
-          throw new Error("APPWRITE_PROJECT_ID is not defined");
-        })()
-    )
-    .setKey(
-      process.env.APPWRITE_API_KEY ??
-        (() => {
-          throw new Error("APPWRITE_API_KEY is not defined");
-        })()
-    );
+    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers["x-appwrite-key"] ?? "");
   const users = new Users(client);
 
   try {
@@ -25,7 +15,7 @@ export default async ({ req, res, log, error }: any) => {
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
     log(`Total users: ${response.total}`);
-  } catch (err: any) {
+  } catch (err) {
     error("Could not list users: " + err.message);
   }
 
