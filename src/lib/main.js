ac62f6803192aec9c2dd95793870f7798d307e49
stiehle <gstiehle@yahoo.de>
2025-06-27 20:59:06 +0200

    Update API key retrieval to use APPWRITE_TESTKEY from request headers

--- src/lib/main.js
+++ src/lib/main.js
@@ -17,7 +17,8 @@ module.exports = async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers["x-appwrite-key"] ?? "");
+    .setKey(req.headers["APPWRITE_TESTKEY"] ?? "");
+  // .setKey(req.headers["x-appwrite-key"] ?? "");
   // .setKey(process.env.APPWRITE_TESTKEY); // Dein API-Schlüssel (Environment Variable)
   // .setKey(process.env.APPWRITE_API_KEY); // Dein API-Schlüssel (Environment Variable)
 
